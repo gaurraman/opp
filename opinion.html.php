739a4c12a29163bbfaf1a8f63f75c5c95b3636fc
Raman Gaur <ramangaursharma@gmail.com>
2019-11-04 18:51:27 +0530

    first commit

---
+++ opinion.html.php
@@ -0,0 +1,38 @@
+<html>
+
+    <head>
+
+        <title>JavaScript Libraries - Opinion Poll</title>
+
+    </head>
+
+    <body bgcolor="#56646">
+
+        <h2 align="center">Opinion Poll</h2>
+		
+		<br>
+		<br>
+		<br>
+		<br>
+
+        <p align="center" size="12"><b>Who is your favourite author?</b></p>
+
+        <form method="POST" action="index.php" align="center">
+
+            <p> <input type="radio" name="vote" value="1" />Miguel de Cervantes
+
+                <br /><input type="radio" name="vote" value="2" />Charles Dickens
+
+                <br /><input type="radio" name="vote" value="3" />J.R.R. Tolkien
+
+                <br /><input type="radio" name="vote" value="4" />Antoine de Saint-Exuper
+
+            </p>
+
+            <p><input type="submit" name="submitbutton" value="OK" /></p>
+
+        </form>
+
+    </body>
+
+</html>
---
+++ results.html.php
@@ -0,0 +1,25 @@
+<html>
+
+    <head>
+
+        <title>Poll Results</title>
+
+    </head>
+
+    <body>
+
+        <h2>Opinion Poll Results</h2>
+
+        <p><b>Who is your favourite author?</b></p>
+
+        <p><b><?php echo $choices_count[0]; ?></b> people have thus far taken part in this poll:</p>
+
+        <p>
+<table>
+
+            <?php echo($table_rows); ?>
+
+        </table>
+</body>
+
+</html>
